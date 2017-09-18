9185dbac130e5484472e7eb905b949f28a38619e
oddalot <oddalot@yahoo.com>
2017-09-17 21:56:41 -0400

    add gradient background

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,19 @@ export default {
 </script>
 
 <style>
+html {
+  height: 100%;
+}
+body {
+  height: 100%;
+  background:
+    radial-gradient(circle at 50% 0,
+         rgba(255,0,0,.5), rgba(0,255,0,0) 70.71%),
+    radial-gradient(circle at 6.7% 75%,
+         rgba(0,0,255,.5), rgba(0,0,255,0) 70.71%),
+    radial-gradient(circle at 93.3% 75%,
+         rgba(0,255,0,.5), rgba(255,0,0,0) 70.71%);
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
